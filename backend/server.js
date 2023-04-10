ca79afabe18abfa25d775f84ceccfe6e44da7306
Rahul <128124860+rahul8033@users.noreply.github.com>
2023-04-11 05:19:14 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -111,11 +111,11 @@ io.on('connection', (socket) => {
     })
 })
 
-if(process.env.NOODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,"../frontend/build")));
-    /*app.get('*',(req,res)=>{
+    app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,"../","frontend","build","index.html"));
-    })*/
+    })
 
 }
 
